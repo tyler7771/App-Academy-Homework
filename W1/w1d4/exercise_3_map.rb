ca690cd7b77c91cb78741a10f8ba6e8aa09d6598
Tyler Fields <TylerFields@tylers-mbp.gateway.sonic.net>
2016-09-15 22:54:47 -0700

    Finish map exercise

--- W1/w1d4/exercise_3_map.rb
+++ W1/w1d4/exercise_3_map.rb
@@ -1,23 +1,24 @@
-class Array
+class Map
+  def initialize
+    @map = []
+  end
+
   def assign(key, value)
-    self.each do |current_key|
+    @map.each do |current_key|
       if current_key[0] == key
         current_key[1] = value
       end
     end
-    self << [key, value] unless self.include?([key, value])
+    @map << [key, value] unless @map.include?([key, value])
+    @map
   end
 
   def delete(key)
-    self.each_with_index do |current_key, idx|
-      if current_key[0] == key
-        self.delete(idx)
-      end
-    end
+    @map.reject! { |pair| pair[0] == key }
   end
 
   def lookup(key)
-    self.each do |current_key|
+    @map.each do |current_key|
       if current_key[0] == key
         return current_key[1]
       end
@@ -25,10 +26,11 @@ class Array
   end
 end
 
-new_array = [["key1", "value1"], ["key2", "value2"], ["key3", "value4"],]
-p new_array
+new_array = Map.new
+new_array.assign("key1", "value1")
+new_array.assign("key2", "value2")
+new_array.assign("key3", "value4")
 p new_array.assign("key3", "value3")
 p new_array.assign("key4", "value4")
 p new_array.delete("key4")
-p new_array
 p new_array.lookup("key2")
