f6e9059fee67b13d264cec891372b5b7ee4f7bf0
tyler7771 <tyler.fields777@gmail.com>
2016-10-07 08:14:56 -0700

    W4D5 homework

---
+++ W4/ToyApp/db/migrate/20161007150200_toys.rb
@@ -0,0 +1,10 @@
+class Toys < ActiveRecord::Migration
+  def change
+    create_table :toys do |t|
+      t.string :name, null: false, unique: true
+      t.references :toyable, polymorphic: true, index: true
+      t.timestamps null: false
+    end
+    add_index :toys, :name
+  end
+end
